3de0cdb3d64e1ee34373bb3d589ebf0906e69e1b
BudzioT <budzio.zsm1@gmail.com>
2025-03-08 00:52:20 +0100

    ee emails startin

---
+++ extension/components/emails/EmailPage.js
@@ -0,0 +1,8 @@
+export default function EmailPage() {
+    return (
+        <div id="email_page">
+            <h4>Emails</h4>
+            <p>Stay safe and let HoneyHive check your email against scams, phishing attempts and more!</p>
+        </div>
+    );
+}
