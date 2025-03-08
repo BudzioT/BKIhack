1ecd5174ff8c075eed44253e7a1500e67dec6c3e
HubertKr4jnik <hkrajnik714@gmail.com>
2025-03-08 06:55:29 +0100

    styled password checker

--- website/src/app/pass/page.tsx
+++ website/src/app/pass/page.tsx
@@ -233,8 +233,9 @@ const PasswordInputWithStrengthChecker: React.FC = () => {
   const [password, setPassword] = useState('');
   
   return (
-    <div className="max-w-md mx-auto p-6 bg-white rounded-lg shadow-md">
-      <h2 className="text-xl font-bold mb-4">Create Password</h2>
+    <div className='min-h-screen flex justify-center place-items-center'>
+      <div className="max-w-md mx-auto p-10 bg-white rounded-lg shadow-md">
+      <h2 className="text-xl font-bold mb-4">Check Password</h2>
       
       <div className="mb-6">
         <label htmlFor="password" className="block text-sm font-medium text-gray-700 mb-1">
@@ -245,12 +246,13 @@ const PasswordInputWithStrengthChecker: React.FC = () => {
           type="password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
-          className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+          className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-amber-400"
           placeholder="Enter your password"
         />
       </div>
       
       <PasswordStrengthChecker password={password} />
+    </div>
     </div>
   );
 };
