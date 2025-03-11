10afda9543c7f59e74e946eff74b054d4097b80b
HubertKr4jnik <h.krajnik714@gmail.com>
2025-03-11 21:46:18 +0100

    LET THE BEE BE BACK

--- website/src/app/layout.tsx
+++ website/src/app/layout.tsx
@@ -30,7 +30,6 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gray-900`}
       >
-        {/* <ImageMetadataViewer/> */}
         {children}
       </body>
     </html>
